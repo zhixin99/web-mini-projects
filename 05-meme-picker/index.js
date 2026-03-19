dbb4222dc189574a56296edf7b73703f238b6968
Zhixin <zhixin.zhu99@gmail.com>
2026-03-19 10:20:30 +0100

    update the data path

--- 05-meme-picker/index.js
+++ 05-meme-picker/index.js
@@ -1,4 +1,4 @@
-import { catsData } from '/data.js'
+import { catsData } from './data.js'
 
 const emotionRadios = document.getElementById('emotion-radios')
 const getImageBtn = document.getElementById('get-image-btn')
